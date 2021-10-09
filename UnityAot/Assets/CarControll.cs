3215f2058eb04b4afbf19a7a35a1e9815a2a397b
Pattrawut1 <87476142+Pattrawut1@users.noreply.github.com>
2021-10-09 17:30:42 +0700

    Update1

---
+++ UnityAot/Assets/CarControll.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControll : MonoBehaviour
+{
+  public float Movespeed = 25f;
+  public float Turnspeed = 0.8f;
+  public Rigidbody rb= null;
+  
+}
